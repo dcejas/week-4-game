bac02a3078bafe975da30c1818f9a3052b86bf4c
David Cejas <davidcejas@Davids-MacBook-Pro.local>
2018-02-18 21:54:43 -0500

    gem files linked and sized

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,4 +4,12 @@ function generateComputerRandom() {
     randomNumber.innerHTML = Math.floor((Math.random() * 101) + 19);
 }
 
-window.onload = generateComputerRandom;
+    window.onload = generateComputerRandom;
+
+$( document ).ready(funtion() {
+    
+    function generateComputerRandom() {
+        var randomNumber = document.getElementById("computerNumber")
+        randomNumber.innerHTML = Math.floor((Math.random() * 101) + 19);
+    }
+});
