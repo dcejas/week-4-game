8f3b18c67889c4cf6ab2928c1b4836f679f29024
David Cejas <davidcejas@Davids-MacBook-Pro.local>
2018-02-18 22:42:04 -0500

    gems generate random number now; still needs tally function and more

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,30 @@
-//Random Number Generator
+//Random Computer Generated Number
 function generateComputerRandom() {
     var randomNumber = document.getElementById("computerNumber")
     randomNumber.innerHTML = Math.floor((Math.random() * 101) + 19);
 }
-
     window.onload = generateComputerRandom;
 
-$( document ).ready(funtion() {
-    
-    function generateComputerRandom() {
-        var randomNumber = document.getElementById("computerNumber")
-        randomNumber.innerHTML = Math.floor((Math.random() * 101) + 19);
-    }
+
+
+
+// $( document ).ready(funtion() {
+
+//     function generateCGemRandom() {
+//         var randomGemNumber = document.getElementById("computerNumber")
+//         randomGemNumber.innerHTML = Math.floor((Math.random() * 101) + 19);
+//     }
+// });
+
+$(document).ready(function() {
+
+    // When random-button is clicked...
+    $(".gems").on("click", function() {
+
+        // ... we generate a random number
+        var gemRandom = Math.floor(Math.random() * 11) + 1;
+
+        // ... and then dump the random number into our random-number div. 
+        $("#crystalValue").text(gemRandom);
+    });
 });
