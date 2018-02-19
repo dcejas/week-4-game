a5702cae193180bd53385bfa373805d44afb8bda
David Cejas <davidcejas@Davids-MacBook-Pro.local>
2018-02-19 00:16:28 -0500

    1st gem tally is working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,16 +21,61 @@ $( document ).ready(function(){
     //  Decaring variables for tallies
     // When gem is clicked...
 
-    
-    $(".gems").on("click", function() {
+    $('#numberWins').text(wins);
+    $('#numberLosses').text(losses);
+    //resets the game
 
-        // ... the gem generates a random number from 1-12
-        var gemRandom = Math.floor(Math.random() * 11) + 1;
+    function reset(){
+        Random=Math.floor(Math.random()*101+19);
+        console.log(Random)
+        $('#computerNumber').text(Random);
+        num1= Math.floor(Math.random()*11+1);
+        num2= Math.floor(Math.random()*11+1);
+        num3= Math.floor(Math.random()*11+1);
+        num4= Math.floor(Math.random()*11+1);
+        userTotal= 0;
+        $('#crystalValue').text(userTotal);
+        } 
+    //adds the wins to the userTotal
 
-        // ... and then dumps the random number into our crystalValue div. 
-        $("#crystalValue").text(gemRandom);
+    function yay(){
+        alert("You won!");
+          wins++; 
+          $('#numberWins').text(wins);
+          reset();
+        }
+        //addes the losses to the userTotal
+        function loser(){
+        alert ("You lose!");
+          losses++;
+          $('#numberLosses').text(losses);
+          reset()
+        }
 
-        var v = $(gemRandom).val();
-        var total = $('#crystalValue').val($('#crystalValue').val()+ v);
-    });
+        //sets up click for jewels
+    $('#diamond').on ('click', function(){
+        userTotal = userTotal + num1;
+        console.log("New userTotal= " + userTotal);
+        $('#crystalValue').text(userTotal); 
+                //sets win/lose conditions
+            if (userTotal == Random){
+                yay();
+            }
+            else if ( userTotal > Random){
+                loser();
+            }   
+    })  
+
+
+    // $(".gems").on("click", function() {
+
+    //     // ... the gem generates a random number from 1-12
+    //     var gemRandom = Math.floor(Math.random() * 11) + 1;
+
+    //     // ... and then dumps the random number into our crystalValue div. 
+    //     $("#crystalValue").text(gemRandom);
+
+    //     var v = $(gemRandom).val();
+    //     var total = $('#crystalValue').val($('#crystalValue').val()+ v);
+    // });
 });
