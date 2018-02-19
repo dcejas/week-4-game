3c9724513467ac44b13207991f595d331f24f72d
David Cejas <davidcejas@Davids-MacBook-Pro.local>
2018-02-18 23:46:46 -0500

    setting vars for each gem individually; and userTotals, wins, and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,24 @@ $( document ).ready(function(){
     // Number should be should be between 19 - 120
     //
     $('#computerNumber').text(Random);
-    // Appending random number to the randomNumber id in the html doc
+    // Appending random number to the computerNumber id in the html doc
     //
 
 
+    var num1= Math.floor(Math.random()*11+1)
+    var num2= Math.floor(Math.random()*11+1)
+    var num3= Math.floor(Math.random()*11+1)
+    var num4= Math.floor(Math.random()*11+1)
+    // Setting up random numbers for each gem
+    // Random number has to be between 1 - 12
+    // 
+    var userTotal= 0; 
+    var wins= 0;
+    var losses = 0;
+    //  Decaring variables for tallies
     // When gem is clicked...
+
+    
     $(".gems").on("click", function() {
 
         // ... the gem generates a random number from 1-12
