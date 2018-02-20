0deb85590a2dec9bd2c644d3158388cabb35106b
dcejas <dcejas430@gmail.com>
2018-02-20 00:30:05 -0500

    changed gems, background; set area colors; moving functions to new boxes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,8 +23,8 @@ $( document ).ready(function(){
     //  Decaring variables for tallies
     // When gem is clicked...
 
-    $('#numberWins').text(wins);
-    $('#numberLosses').text(losses);
+    $('#numberWins').text("Wins: " + wins);
+    $('#numberLosses').text("Losses: " + losses);
     //resets the game
 
     function reset(){
@@ -43,19 +43,19 @@ $( document ).ready(function(){
     function yay(){
         alert("You won!");
           wins++; 
-          $('#numberWins').text(wins);
+          $('#numberWins').text("Wins: " + wins);
           reset();
         }
         //addes the losses to the userTotal
         function loser(){
         alert ("You lose!");
           losses++;
-          $('#numberLosses').text(losses);
+          $('#numberLosses').text("Losses: " + losses);
           reset()
         }
 
         //sets up click for jewels
-    $('#silverGem').on ('click', function(){
+    $('#purpleGem').on ('click', function(){
         userTotal = userTotal + num1;
         console.log("New userTotal= " + userTotal);
         $('#crystalValue').text(userTotal); 
